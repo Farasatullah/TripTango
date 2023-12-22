2f80f1945b6610dbedbd52d81ef6328675c50a9d
farasat <farasatullahk@gmail.com>
2023-12-22 13:03:27 +0500

    Initial Commit

---
+++ TripTango/TripTangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripTangoApp.swift
+//  TripTango
+//
+//  Created by Farasat_Niazi on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TripTangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
